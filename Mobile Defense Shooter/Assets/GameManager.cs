16d9ca8de681b88eca3c987dc7b781c8202ba150
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-11-11 12:18:14 -0500

    Adding Player Mesh

---
+++ Mobile Defense Shooter/Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager {
+
+    private GameObject gameObject;
+    private static GameManager m_Instance;
+    public static GameManager Instance {
+        get {
+            if(m_Instance == null) {
+                m_Instance = new GameManager {
+                    gameObject = new GameObject("_gameManager")
+                };
+                m_Instance.gameObject.AddComponent<InputController>();
+                // Add components to Game manager using m_instance.gameObject.AddComponent<DESIRED COMPONENT>();
+
+            }
+            return m_Instance;
+        }
+    }
+
+    private static InputController m_InputController;
+    public static InputController InputController {
+        get{
+            if(m_InputController == null) {
+                m_InputController= m_Instance.gameObject.GetComponent<InputController>();
+            }
+            return m_InputController;
+        }
+        
+    }
+
+}
