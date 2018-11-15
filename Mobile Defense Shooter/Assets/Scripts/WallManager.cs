fc6257107417134f486e2ff87873b01c82219a6b
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-11-14 23:48:52 -0500

    Models & Enemy's !
    Added the place holder models from Matt and the basic Nav Mesh Agent Prefab with the place holder models

--- Mobile Defense Shooter/Assets/Scripts/WallManager.cs
+++ Mobile Defense Shooter/Assets/Scripts/WallManager.cs
@@ -1,44 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
+using Unity.Collections;
 
 public class WallManager : MonoBehaviour {
 
     [SerializeField]
     private GameObject wallSegmentPrefab;
+    public List<GameObject> wallSegments = new List<GameObject>();
 
-    public int wallSegmentsPerFace =5;
-    public int wallFaces = 4;
-    public float spacing=5;
+    private void Awake() {
+        //int segmentCount = 
+    }
 
     public void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            BuildWall();
-        }
+
     }
 
-
-    public void BuildWall() {
-
-        // For each wall face -> Do
-            // create parent GameObject for wall face
-        // For each wall Segment -> Do
-            // Instantiate segments for wall face and set to parent
-            // move the block to it's position on the wall face
-        for(int f=0; f<wallFaces; f++) {
-            GameObject wall = new GameObject("wallFace_" + f);
-            for(int s=0; s<wallSegmentsPerFace; s++) {
-                GameObject segment = Instantiate(wallSegmentPrefab);
-                segment.transform.SetParent(wall.transform);
-                Vector3 offset = new Vector3(segment.transform.position.x, segment.transform.position.y, segment.transform.position.z+(spacing*s));
-                segment.transform.position = offset;
-            }
-            Vector3 wallOffset = new Vector3(wall.transform.position.x, wall.transform.position.y, wall.transform.position.z + (f));
-            wall.transform.position = wallOffset;
-            wall.transform.Rotate(new Vector3(0, 90 * f, 0));
-        }
-    
-    }
 
 }
