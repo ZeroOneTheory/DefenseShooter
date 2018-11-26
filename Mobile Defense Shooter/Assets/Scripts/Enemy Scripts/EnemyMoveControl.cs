c865936201c33fb97cddb428a93a65418501ac83
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-11-25 19:28:52 -0500

    Enemy Move Controller Added

--- Mobile Defense Shooter/Assets/Scripts/Enemy Scripts/EnemyMoveControl.cs
+++ Mobile Defense Shooter/Assets/Scripts/Enemy Scripts/EnemyMoveControl.cs
@@ -6,39 +6,57 @@ using UnityEngine;
 public class EnemyMoveControl : MonoBehaviour {
 
     public Enemy enemy;
-    public Transform tar;
-    
+    public Rigidbody rb;
 
-    private void Start() { 
+    private Vector3 direction;
+    private Quaternion lookRotation;
+
+    public bool canMove=false;
+    private Transform target;
+    private float moveRate;
+
+
+    private void Start() {
         enemy = GetComponentInParent<EnemyController>().enemyTemplate;
-        tar = GetComponentInParent<EnemyController>().target;
+        target = GetComponentInParent<EnemyController>().target;
+        rb = GetComponent<Rigidbody>();
+
         
-
-        DecideMovement();
     }
 
-    public void DecideMovement() {
-        StartCoroutine(ExecuteMovment(enemy.moveType, tar));
+    public void Update() {
+        direction = target.position - transform.position;
+        lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, enemy.rotationSpeed * Time.deltaTime);
+
+        if (!canMove) {
+            if (Random.Range(0,3)==1) {
+                SetMovement();
+                canMove = true;
+            }
+        }
     }
 
-    public IEnumerator ExecuteMovment(AIMoveType moveType, Transform target) {
-        switch (moveType) {
+    public void SetMovement() {
+        rb.velocity = Vector3.zero;
+        StartCoroutine(ExecuteMove(enemy.moveType));
+    }
 
-            case AIMoveType.towards: 
-                        Vector3 direction = target.position - transform.position; 
-                        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x,0f, direction.z));
-                        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, enemy.rotationSpeed* Time.deltaTime);
-                        transform.Translate(Vector3.forward * enemy.moveSpeed * Time.deltaTime);
-                yield return new WaitForSeconds(Random.Range(enemy.idleMinMax.x, enemy.idleMinMax.y));
-                break;
-               
+    public IEnumerator ExecuteMove(AIMoveType moveType) {
+
+        
+        if (moveType == AIMoveType.towards) {
+            rb.drag = enemy.moveSpeed * .001f;
+            rb.AddForce(direction.normalized * enemy.moveSpeed);
+        }
+        if (moveType == AIMoveType.inchTowrads) {
+            rb.drag = enemy.moveSpeed*.001f*9f;
+            rb.AddForce(direction.normalized * enemy.moveSpeed);
+            yield return new WaitForSeconds(enemy.idleMinMax.y);
         }
         yield return null;
-
+        SetMovement();
     }
-
-
-
 
 
 }
